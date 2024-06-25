7a9e8da26cfbabbac2fac462e58f4f3f395bb83b
BaronDontinental <170842015+BaronDontinental@users.noreply.github.com>
2024-06-25 09:38:46 -0400

    envy extended

--- TBOI Apostasy/bingus/characters/envy/L21_Envy.lua
+++ TBOI Apostasy/bingus/characters/envy/L21_Envy.lua
@@ -21,6 +21,7 @@ local L21_EnvyStats = {
     TEARCOLOR = Color(0, 0, 0, 0, 0, 0, 0)
 }
 local RNG_SHIFT_INDEX = 35
+
 local fam = {
   dmgCount = 0,
   hitChance = 1,
@@ -28,6 +29,14 @@ local fam = {
   CloseOrbitUpgrade1 = 0,
   CloseOrbitUpgrade2 = 0,
   CloseOrbitUpgrade3 = 0,
+  FarOrbit1 = 0,
+  FarOrbit2 = 0,
+  FarOrbit3 = 0,
+  FarOrbit4 = 0,
+  ZigUpgrade1 = 0,
+  ZigUpgrade2 = 0,
+  ZigUpgrade3 = 0,
+  ZigUpgrade4 = 0,
   chance = 10,
   tryspawn = false,
   Close1 = 0,
@@ -38,8 +47,8 @@ local fam = {
   Far3 = 0,
   ZigZag1 = 0,
   ZigZag2 = 0,
-  ZigZag3 = 0
-  
+  ZigZag3 = 0,
+  trycount = 0
 }
 
 function L21_Envy:postUpdate()
@@ -75,10 +84,43 @@ function L21_Envy:postUpdate()
         end
     end
     mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, L21_Envy.OnCache)
+    
     function L21_Envy:EUpdate(player)
+      if(Game:GetFrameCount() == 1) then
+        fam = {
+          dmgCount = 0,
+          hitChance = 1,
+          CloseOrbitCount = 0,
+          CloseOrbitUpgrade1 = 0,
+          CloseOrbitUpgrade2 = 0,
+          CloseOrbitUpgrade3 = 0,
+          FarOrbit1 = 0,
+          FarOrbit2 = 0,
+          FarOrbit3 = 0,
+          FarOrbit4 = 0,
+          ZigUpgrade1 = 0,
+          ZigUpgrade2 = 0,
+          ZigUpgrade3 = 0,
+          ZigUpgrade4 = 0,
+          chance = 10,
+          tryspawn = false,
+          Close1 = 0,
+          Close2 = 0,
+          Close3 = 0,
+          Far1 = 0,
+          Far2 = 0,
+          Far3 = 0,
+          ZigZag1 = 0,
+          ZigZag2 = 0,
+          ZigZag3 = 0,
+          trycount = 0 
+          }
+      end
+      
       if player:GetPlayerType() ~= EnvyGuy then
         return
-      end
+      end 
+      
       if fam.dmgCount == 1 then
         fam.hitChance = fam.hitChance + 1
         fam.dmgCount = fam.dmgCount - 1
@@ -88,8 +130,8 @@ function L21_Envy:postUpdate()
             fam.tryspawn = true
           end
       end
-
       if fam.tryspawn then
+        fam.trycount = 0
         fam.tryspawn = false
         local EnvyFamType = math.random(3)
           if EnvyFamType == 1 then
@@ -106,7 +148,11 @@ function L21_Envy:postUpdate()
                     fam.CloseOrbitUpgrade2 = fam.CloseOrbitUpgrade2 - 1
                     fam.CloseOrbitUpgrade3 = fam.CloseOrbitUpgrade3 + 1
                     elseif fam.Close1 >= 5 then
-                      fam.tryspawn = true
+                      EnvyFamType = 2
+                      fam.trycount = fam.trycount + 1
+                      if fam.trycount == 9 then
+                          return
+                      end  
                     end
           elseif EnvyFamType == 2 then
             fam.Close2 = fam.Close2 + 1
@@ -122,7 +168,11 @@ function L21_Envy:postUpdate()
                     fam.CloseOrbitUpgrade2 = fam.CloseOrbitUpgrade2 - 1
                     fam.CloseOrbitUpgrade3 = fam.CloseOrbitUpgrade3 + 1
                     elseif fam.Close2 >= 5 then
-                      fam.tryspawn = true
+                      EnvyFamType = 3
+                      fam.trycount = fam.trycount + 1
+                      if fam.trycount == 9 then
+                          return
+                      end  
                     end
           elseif EnvyFamType == 3 then
             fam.Close3 = fam.Close3 + 1
@@ -138,26 +188,132 @@ function L21_Envy:postUpdate()
                     fam.CloseOrbitUpgrade2 = fam.CloseOrbitUpgrade2 - 1
                     fam.CloseOrbitUpgrade3 = fam.CloseOrbitUpgrade3 + 1
                     elseif fam.Close3 >= 5 then
-                      fam.tryspawn = true
+                      EnvyFamType = 4
+                      fam.trycount = fam.trycount + 1
+                      if fam.trycount == 9 then
+                          return
+                      end  
                     end
           elseif EnvyFamType == 4 then
             fam.Far1 = fam.Far1 + 1
-
+            if fam.Far1 == 1 then
+              fam.FarOrbit1 = fam.FarOrbit1 + 1
+              elseif fam.Far1 == 2 then
+                fam.FarOrbit1 = fam.FarOrbit1 - 1
+                fam.FarOrbit2 = fam.FarOrbit2 + 1
+                elseif fam.Far1 == 3 then
+                  fam.FarOrbit2 = fam.FarOrbit2 - 1
+                  fam.FarOrbit3 = fam.FarOrbit3 + 1
+                  elseif fam.Far1 == 4 then
+                    fam.FarOrbit3 = fam.FarOrbit3 - 1
+                    fam.FarOrbit4 = fam.FarOrbit4 + 1
+                    elseif fam.Far1 >= 5 then
+                      EnvyFamType = 5
+                      fam.trycount = fam.trycount + 1
+                      if fam.trycount == 9 then
+                          return
+                      end  
+                    end
           elseif EnvyFamType == 5 then
             fam.Far2 = fam.Far2 + 1
-
+            if fam.Far2 == 1 then
+              fam.FarOrbit1 = fam.FarOrbit1 + 1
+              elseif fam.Far2 == 2 then
+                fam.FarOrbit1 = fam.FarOrbit1 - 1
+                fam.FarOrbit2 = fam.FarOrbit2 + 1
+                elseif fam.Far2 == 3 then
+                  fam.FarOrbit2 = fam.FarOrbit2 - 1
+                  fam.FarOrbit3 = fam.FarOrbit3 + 1
+                  elseif fam.Far2 == 4 then
+                    fam.FarOrbit3 = fam.FarOrbit3 - 1
+                    fam.FarOrbit4 = fam.FarOrbit4 + 1
+                    elseif fam.Far2 >= 5 then
+                      EnvyFamType = 6
+                      fam.trycount = fam.trycount + 1
+                      if fam.trycount == 9 then
+                          return
+                      end  
+                    end
           elseif EnvyFamType == 6 then
             fam.Far3 = fam.Far3 + 1
-
+            if fam.Far3 == 1 then
+              fam.FarOrbit1 = fam.FarOrbit1 + 1
+              elseif fam.Far3 == 2 then
+                fam.FarOrbit1 = fam.FarOrbit1 - 1
+                fam.FarOrbit2 = fam.FarOrbit2 + 1
+                elseif fam.Far3 == 3 then
+                  fam.FarOrbit2 = fam.FarOrbit2 - 1
+                  fam.FarOrbit3 = fam.FarOrbit3 + 1
+                  elseif fam.Far3 == 4 then
+                    fam.FarOrbit3 = fam.FarOrbit3 - 1
+                    fam.FarOrbit4 = fam.FarOrbit4 + 1
+                    elseif fam.Far3 >= 5 then
+                      EnvyFamType = 7
+                      fam.trycount = fam.trycount + 1
+                      if fam.trycount == 9 then
+                          return
+                      end  
+                    end
           elseif EnvyFamType == 7 then
             fam.ZigZag1 = fam.ZigZag1 + 1
-
+            if fam.ZigZag1 == 1 then
+              fam.ZigUpgrade1 = fam.ZigUpgrade1 + 1
+              elseif fam.ZigZag1 == 2 then
+                fam.ZigUpgrade1 = fam.ZigUpgrade1 - 1
+                fam.ZigUpgrade2 = fam.ZigUpgrade2 + 1
+                elseif fam.ZigZag1 == 3 then
+                  fam.ZigUpgrade2 = fam.ZigUpgrade2 - 1
+                  fam.ZigUpgrade3 = fam.ZigUpgrade3 + 1
+                  elseif fam.ZigZag1 == 4 then
+                    fam.ZigUpgrade3 = fam.ZigUpgrade3 - 1
+                    fam.ZigUpgrade4 = fam.ZigUpgrade4 + 1
+                    elseif fam.ZigZag1 >= 5 then
+                      EnvyFamType = 8
+                      fam.trycount = fam.trycount + 1
+                      if fam.trycount == 9 then
+                          return
+                      end  
+                    end
           elseif EnvyFamType == 8 then
             fam.ZigZag2 = fam.ZigZag2 + 1
-
+            if fam.ZigZag2 == 1 then
+              fam.ZigUpgrade1 = fam.ZigUpgrade1 + 1
+              elseif fam.ZigZag2 == 2 then
+                fam.ZigUpgrade1 = fam.ZigUpgrade1 - 1
+                fam.ZigUpgrade2 = fam.ZigUpgrade2 + 1
+                elseif fam.ZigZag2 == 3 then
+                  fam.ZigUpgrade2 = fam.ZigUpgrade2 - 1
+                  fam.ZigUpgrade3 = fam.ZigUpgrade3 + 1
+                  elseif fam.ZigZag2 == 4 then
+                    fam.ZigUpgrade3 = fam.ZigUpgrade3 - 1
+                    fam.ZigUpgrade4 = fam.ZigUpgrade4 + 1
+                    elseif fam.ZigZag2 >= 5 then
+                      EnvyFamType = 9
+                      fam.trycount = fam.trycount + 1
+                      if fam.trycount == 9 then
+                          return
+                      end  
+                    end
           elseif EnvyFamType == 9 then
             fam.ZigZag3 = fam.ZigZag3 + 1
-
+            if fam.ZigZag3 == 1 then
+              fam.ZigUpgrade1 = fam.ZigUpgrade1 + 1
+              elseif fam.ZigZag3 == 2 then
+                fam.ZigUpgrade1 = fam.ZigUpgrade1 - 1
+                fam.ZigUpgrade2 = fam.ZigUpgrade2 + 1
+                elseif fam.ZigZag3 == 3 then
+                  fam.ZigUpgrade2 = fam.ZigUpgrade2 - 1
+                  fam.ZigUpgrade3 = fam.ZigUpgrade3 + 1
+                  elseif fam.ZigZag3 == 4 then
+                    fam.ZigUpgrade3 = fam.ZigUpgrade3 - 1
+                    fam.ZigUpgrade4 = fam.ZigUpgrade4 + 1
+                    elseif fam.ZigZag3 >= 5 then
+                      EnvyFamType = 1
+                      fam.trycount = fam.trycount + 1
+                      if fam.trycount == 9 then
+                          return
+                      end  
+                    end
           end
       end
       player:AddCacheFlags(CacheFlag.CACHE_FAMILIARS)
@@ -175,6 +331,9 @@ function L21_Envy:postUpdate()
 
   ---@param player EntityPlayer
     function L21_Envy:ChacheFam(player)
+      if player:GetPlayerType() ~= EnvyGuy then
+        return
+      end
       local rng = RNG()
       local seed = math.max(Random(), 1)
       rng:SetSeed(seed, RNG_SHIFT_INDEX)
@@ -185,8 +344,12 @@ function L21_Envy:postUpdate()
     end
     mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, L21_Envy.ChacheFam, CacheFlag.CACHE_FAMILIARS) 
 
-    ---@param familiar EntityFamiliar
+  ---@param familiar EntityFamiliar
     function L21_Envy:init(familiar)
+      local player = familiar.Player
+      if player:GetPlayerType() ~= EnvyGuy then
+        return
+      end
       familiar:AddToOrbit(0)
     end
     mod:AddCallback(ModCallbacks.MC_FAMILIAR_INIT, L21_Envy.init, FAMILIAR_ENVY_ORBIT)
@@ -194,12 +357,13 @@ function L21_Envy:postUpdate()
     mod:AddCallback(ModCallbacks.MC_FAMILIAR_INIT, L21_Envy.init, FAMILIAR_ENVY_ORBITV3)
     mod:AddCallback(ModCallbacks.MC_FAMILIAR_INIT, L21_Envy.init, FAMILIAR_ENVY_ORBITV4)
 
-    ---@param familiar EntityFamiliar
+  ---@param familiar EntityFamiliar
     function L21_Envy:UpdateFam(familiar)
       local sprite = familiar:GetSprite()
       local player = familiar.Player
-      local direction
-
+      if player:GetPlayerType() ~= EnvyGuy then
+        return
+      end
       if sprite:IsFinished() then
         sprite:Play("Float")
       end
@@ -211,6 +375,7 @@ function L21_Envy:postUpdate()
     mod:AddCallback(ModCallbacks.MC_FAMILIAR_UPDATE, L21_Envy.UpdateFam, FAMILIAR_ENVY_ORBITV2)
     mod:AddCallback(ModCallbacks.MC_FAMILIAR_UPDATE, L21_Envy.UpdateFam, FAMILIAR_ENVY_ORBITV3)
     mod:AddCallback(ModCallbacks.MC_FAMILIAR_UPDATE, L21_Envy.UpdateFam, FAMILIAR_ENVY_ORBITV4)
+    
     function L21_Envy:OnUpdate()
         local player = Isaac.GetPlayer(0)
 
