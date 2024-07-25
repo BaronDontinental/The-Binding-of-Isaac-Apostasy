0fa6651be21b8f1d4b212e49b20095c56c83a46f
Jackson Terrell <jacksonterrell12@gmail.com>
2024-07-25 18:57:42 -0400

    bug fixes

--- TBOI Apostasy/bingus/Items/Envy/Moar.lua
+++ TBOI Apostasy/bingus/Items/Envy/Moar.lua
@@ -12,26 +12,28 @@ function Moar:postUpdate()
         local copyCount = player:GetCollectibleNum(CollectibleType.COLLECTIBLE_MOAR)
         if copyCount > 0 then
             local entities = Isaac.GetRoomEntities()
+            if game:GetLevel():GetStage() ~= LevelStage.STAGE6 then
                 for _, entity in ipairs(entities) do
                     local data = entity:GetData() 
                      MoarE = entity:ToNPC()
-                if MoarE and MoarE:IsBoss() then
-                    if MoarE:IsDead() and not data.Died then
-                        data.Died = true
-                        Isaac.Spawn(
-                            EntityType.ENTITY_PICKUP,
-                            PickupVariant.PICKUP_CHEST,
-                            ChestSubType.CHEST_CLOSED,
-                            MoarE.Position,
-                            MoarE.Velocity,
-                            nil)
-                    elseif MoarE.ParentNPC or
-                        MoarE.Type == EntityType.ENTITY_BLASTOCYST_MEDIUM
-                        or MoarE.Type == EntityType.ENTITY_BLASTOCYST_SMALL
-                        or MoarE.Type == EntityType.ENTITY_FISTULA_MEDIUM
-                        or MoarE.Type == EntityType.ENTITY_FISTULA_SMALL
-                        or MoarE.Type == EntityType.ENTITY_ENVY then
+                    if MoarE and MoarE:IsBoss() then
+                        if MoarE:IsDead() and not data.Died then
                             data.Died = true
+                            Isaac.Spawn(
+                                EntityType.ENTITY_PICKUP,
+                                PickupVariant.PICKUP_CHEST,
+                                ChestSubType.CHEST_CLOSED,
+                                MoarE.Position,
+                                MoarE.Velocity,
+                                nil)
+                        elseif MoarE.ParentNPC or
+                            MoarE.Type == EntityType.ENTITY_BLASTOCYST_MEDIUM
+                            or MoarE.Type == EntityType.ENTITY_BLASTOCYST_SMALL
+                            or MoarE.Type == EntityType.ENTITY_FISTULA_MEDIUM
+                            or MoarE.Type == EntityType.ENTITY_FISTULA_SMALL
+                            or MoarE.Type == EntityType.ENTITY_ENVY then
+                                data.Died = true
+                        end
                     end
                 end
             end
