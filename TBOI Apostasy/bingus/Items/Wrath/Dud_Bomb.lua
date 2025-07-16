c1afbdc21b08d09d65757ff417ebe6f66de7602d
BaronDontinental <Jacksonterrell12@gmail.com>
2025-07-16 06:57:59 -0400

    dud bomb finished

--- TBOI Apostasy/bingus/Items/Wrath/Dud_Bomb.lua
+++ TBOI Apostasy/bingus/Items/Wrath/Dud_Bomb.lua
@@ -1,10 +1,22 @@
 local Dud_Bomb = {}
 local game = Game()
 
-mod.COLLECTIBLE_DUD_BOMB = Isaac.GetItemIdByName("Dud Bomb")
-CollectibleType.COLLECTIBLE_DUD_BOMB = Isaac.GetItemIdByName("Dud Bomb")
+mod.COLLECTIBLE_DUD_BOMB = Isaac.GetTrinketIdByName("Dud Bomb")
+TrinketType.COLLECTIBLE_DUD_BOMB = Isaac.GetTrinketIdByName("Dud Bomb")
 
 function Dud_Bomb:postUpdate()
+
+    function Dud_Bomb:TakeDmg(player, _, dmgFlag, DmgSourceRef)
+        if player:HasTrinket(TrinketType.COLLECTIBLE_DUD_BOMB) then
+            if DmgSourceRef.Variant == BombVariant.BOMB_TROLL 
+            or DmgSourceRef.Variant == BombVariant.BOMB_SUPERTROLL 
+            or DmgSourceRef.Variant == BombVariant.BOMB_GOLDENTROLL then
+                return false
+            end
+        end
+    end
+    mod:AddCallback(ModCallbacks.MC_PRE_PLAYER_TAKE_DMG, Dud_Bomb.TakeDmg)
+
 end
 
 return Dud_Bomb
