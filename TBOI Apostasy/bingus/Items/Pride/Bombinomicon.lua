4a8282e5c696f33fa2b3fd710524892d7a651fea
BaronDontinental <Jacksonterrell12@gmail.com>
2025-07-16 03:35:07 -0400

    bombinomicon finished and implemented

--- TBOI Apostasy/bingus/Items/Pride/Bombinomicon.lua
+++ TBOI Apostasy/bingus/Items/Pride/Bombinomicon.lua
@@ -5,6 +5,27 @@ mod.COLLECTIBLE_BOMBINOMICON = Isaac.GetItemIdByName("Bombinomicon")
 CollectibleType.COLLECTIBLE_BOMBINOMICON = Isaac.GetItemIdByName("Bombinomicon")
 
 function Bombinomicon:postUpdate()
+
+    function Bombinomicon:TakeDmg(player, _, dmgFlag, DmgSourceRef)
+        if player:HasCollectible(CollectibleType.COLLECTIBLE_BOMBINOMICON)then
+            
+            if DmgSourceRef.Variant == BombVariant.BOMB_TROLL 
+            or DmgSourceRef.Variant == BombVariant.BOMB_SUPERTROLL 
+            or DmgSourceRef.Variant == BombVariant.BOMB_GOLDENTROLL then
+                player:UseCard(Card.CARD_TOWER, UseFlag.USE_NOANNOUNCER)
+            end
+
+
+            if (dmgFlag & DamageFlag.DAMAGE_EXPLOSION == DamageFlag.DAMAGE_EXPLOSION) then
+                print("try")
+                return false
+            end
+        end
+    end
+    mod:AddCallback(ModCallbacks.MC_PRE_PLAYER_TAKE_DMG, Bombinomicon.TakeDmg)
+
+
+
 end
 
 return Bombinomicon
